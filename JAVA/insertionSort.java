e7b037ba490aa8cd8067cfd7c34dd7f82d865022
Naresh Chandanbatve <chandanbatven@gmail.com>
2021-10-14 20:25:54 +0530

    adding insertion and bubble sort java

---
+++ JAVA/insertionSort.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class insertionSort {
+    
+public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+    
+
+// Taking array input //
+System.out.println("Enter the sie of an array :- ");
+int n=sc.nextInt();
+
+int[] arr = new int[n];
+
+System.out.println("Enter "+n+" integers :- ");
+
+for(int i=0 ; i<n; i++) {
+ arr[i]=sc.nextInt();
+
+}
+
+
+// sorting array //
+
+for(int)
+
+
+
+
+
+}
+
+
+
+
+}
