31290fe9b7affb4d30f4aa9c35b0a15e5f56275c
AR-Thube <91319379+AR-Thube@users.noreply.github.com>
2021-10-26 11:23:23 +0530

    Update basic_calculator.py

--- Python/basic_calculator.py
+++ Python/basic_calculator.py
@@ -1,10 +1,16 @@
 print("Welcome to python calculator\n")
 
+
+#Knowing the choice from user about the operation to be performed.
 option = int(input("1.Addition\n2.Substraction\n3.Divition\n4.Multiplication\n\nSelect one : "))
 
+
+#Taking input from the user.
 num1 = float(input("Enter first number : "))
 num2 = float(input("Enter second number : "))
 
+
+#Performing operations based on what user needs.
 if option == 1:
 	print(num1 + num2)
 elif option == 2:
