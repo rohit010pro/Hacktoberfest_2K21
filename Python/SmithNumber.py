b3d0f1f874e665629085b96bb56e8846539de8f4
Sayali <91862740+sayalipanch@users.noreply.github.com>
2021-10-28 16:55:22 +0530

    Update
    Added the condition for when user input is Prime as Smith number is composite by definition

--- Python/SmithNumber.py
+++ Python/SmithNumber.py
@@ -17,28 +17,32 @@ else:
                 continue
         return True
 
-    sum, total = 0, 0
-    for i in range(len(x)):  # To calculate sum of digits 
-        sum += m % 10
-        m //= 10
+    if Prime(n):
+        print("The given number is a prime number and hence, IS NOT a Smith number!\n")
 
-    while n % 2 == 0:   # n is even
-        factors.append(2)
-        n = n / 2
-    for i in range(3,int(math.sqrt(n))+1,2):    # n is odd
-        while n % i == 0:
-            factors.append(int(i))
-            n = int(n / i)           
-    if n > 2:   #n is prime
-        factors.append(int(n))
+    else:
+        sum, total = 0, 0
+        for i in range(len(x)):  # To calculate sum of digits 
+            sum += m % 10
+            m //= 10
 
-    for elem in factors:
-        if len(str(elem))>1:
-            for i in range(len(str(elem))): 
-                total+=elem%10
-                elem//=10
-        else:
-            total+=elem
+        while n % 2 == 0:   # n is even
+            factors.append(2)
+            n = n / 2
+        for i in range(3,int(math.sqrt(n))+1,2):    # n is odd
+            while n % i == 0:
+                factors.append(int(i))
+                n = int(n / i)           
+        if n > 2:   #n is prime
+            factors.append(int(n))
+
+        for elem in factors:
+            if len(str(elem))>1:
+                for i in range(len(str(elem))): 
+                    total+=elem%10
+                    elem//=10
+            else:
+                total+=elem
     # Final Output display
         print("The prime factors of the number are:",factors )
         print("The sum of its digits is:",sum)
